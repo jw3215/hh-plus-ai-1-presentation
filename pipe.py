fbd9138cf99904ada595e6f2f21a6a91f9727f75
shinjw <jongwoo.shin@teamsparta.co>
2024-11-10 12:18:23 +0900

    fill in code

--- pipe.py
+++ pipe.py
@@ -1,7 +1,12 @@
 from pipe_helper import pipe_decorator
 
+@pipe_decorator
 def inc(x):
     return x + 1
 
+@pipe_decorator
 def square(x):
     return x**2
+
+
+print((1 | inc | square))  # 4
--- tensor_add.py
+++ tensor_add.py
@@ -1,7 +1,8 @@
 import torch
 
 class MyTensor(list):
-    pass
+    def __add__(self, other):
+        return MyTensor([a + b for a, b in zip(self, other)])
 
 print(torch.Tensor([1, 2, 3]) + torch.Tensor([4, 5, 6]))
 print(MyTensor([1, 2, 3]) + MyTensor([4, 5, 6]))
